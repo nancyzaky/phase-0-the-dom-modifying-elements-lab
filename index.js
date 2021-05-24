7b02e81753e2f464c9161cc3d563ce352d740144
Nancy <nancyzaky29@gmail.com>
2021-05-23 22:46:57 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const removed = document.getElementById('main')
+removed.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id="victory"
+newHeader.innerHTML= 'Nancy is the champion';
